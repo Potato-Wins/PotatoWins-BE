709bc34c71c3eddb143c9d04aeb0740eb0ce340a
sunny <122611609+sunnyanna0@users.noreply.github.com>
2024-11-20 17:05:34 +0900

    🎉tada:초기세팅

---
+++ src/test/java/com/example/potatowinsbe/PotatoWinsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.potatowinsbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotatoWinsBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
